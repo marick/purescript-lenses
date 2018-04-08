135f86adf302fd039ed80ce005ad6898f55b6339
Brian Marick <marick@exampler.com>
2018-04-08 11:25:39 -0500

    Add a stringifier.

--- src/Critter4Us/Model.purs
+++ src/Critter4Us/Model.purs
@@ -1,7 +1,10 @@
 module Critter4Us.Model
   ( Model
   , initialModel
+  , addAnimal
   , addAnimalTag
+
+  , grr
   )
   where
 
@@ -10,9 +13,12 @@ import Critter4Us.Animal (Animal)
 import Critter4Us.Animal as Animal
 import Data.Map (Map)
 import Data.Map as Map
-import Data.Lens (Lens', lens, over)
+import Data.Lens (Lens', lens, over, setJust)
 import Data.Lens.At (at)
 import Data.Maybe (Maybe)
+import Data.Record.ShowRecord
+import Data.Foldable
+import Data.Maybe
 
 type Model =
   { animals :: Map Animal.Id Animal
@@ -26,6 +32,9 @@ addAnimalTag :: Animal.Id -> String -> Model -> Model
 addAnimalTag id tag =
   updateAnimal id (Animal.addTag tag)
 
+addAnimal :: Animal.Id -> String -> Model -> Model
+addAnimal id name =
+  setJust (oneAnimal id) (Animal.named name id)
 
 {- Internal -}
 
@@ -44,3 +53,16 @@ updateAnimal id f model =
 startingAnimal :: Animal
 startingAnimal =
   Animal.named "Genesis" 3838 # Animal.addTag "mare"
+
+
+{- Debug -}
+
+-- `Show` a model when maps and records don't implement `Show`
+-- I trust there's a better way.
+grr :: Model -> String
+grr {animals} =
+  foldl step "" (Map.keys animals)
+  where
+    step acc k =
+      acc <> " (" <> show k <> "=>" <> maybe "!?" showRecord (Map.lookup k animals)
+
