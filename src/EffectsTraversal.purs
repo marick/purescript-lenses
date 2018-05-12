544303241f165e7c633e3549f52c82b486eb83ec
Brian Marick <marick@exampler.com>
2018-05-12 16:05:22 -0500

    A few more examples

--- src/EffectsTraversal.purs
+++ src/EffectsTraversal.purs
@@ -20,7 +20,33 @@ import Data.Lens
 import Data.Lens as Lens
 import Control.Monad.Eff.Random
 import Data.Traversable
+import Data.Maybe
 
 invertified = traverse (randomInt 0) [10, 20, 30] -- not the lens version
 -- [6,7,11]
+
+
+{- 
+sequenceOf traversed $ map (randomInt 3) [10, 20, 30]
+
+
+> (traverseOf traversed pure [10, 20, 30]) :: Maybe (Array Int)
+(Just [10,20,30])
+
+> pure [10, 20, 30] :: Maybe (Array Int)
+(Just [10,20,30])
+
+
+traverseOf traversed pure ≡ pure
+
+-}
+
+-- t :: (Int -> Maybe Int) -> Array Int -> Maybe (Array Int)
+t = traverseOf traversed
+
+t1 :: forall f. Applicative f => f Int -> Maybe (f Int)
+t1 = Just
+
+t2 :: Int -> Maybe Int
+t2 = Just <<< ((-) 3)
 
